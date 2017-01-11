7912f1ada9f94c8f52de1c2f89b774858c095b0f
wjwelchusa <wwelch@ctcd.edu>
2017-01-11 11:15:11 -0600

    Began functions for cat picture app.

--- Python_JS_AP6/driver.py
+++ Python_JS_AP6/driver.py
@@ -1,7 +1,43 @@
-# Baseline Python Jump Start Application 6
+# Baseline Python Jump Start Application 6 - Cat Factory
 # Joe Welch
 # 11 Jan 2017
 # LOL Cat Factory
 
+# Learned in this App:
+# HTTP clients
+# Binary files
+# Subprocesses
+# File and folder management
 
-## Test code to ensure merge to GitHub works
+import os
+
+def main():
+    print_header()
+    folder = get_or_create_output_folder()
+    print(folder)
+    # download cats
+    # display cats
+
+def print_header():
+    print("--------------------------------")
+    print("         (CAT FACTORY)          ")
+    print("--------------------------------")
+
+def get_or_create_output_folder():
+    base_folder = os.path.dirname(__file__)
+    folder = "cat_pictures"
+    full_path = os.path.join(base_folder, folder)
+    print (full_path)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print("Creating new directory at {}".format(full_path))
+        os.mkdir(full_path)
+
+    return full_path
+
+
+
+
+
+if __name__ == '__main__':
+    main()
